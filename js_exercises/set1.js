c06ed25a6f6e921e04ac51ea51f1a78fdd4761b3
bradcgarner <brad@bradgarner.com>
2017-08-21 14:30:14 -0400

    edited set1.js

--- js_exercises/set1.js
+++ js_exercises/set1.js
@@ -1,2 +1,19 @@
-console.log("Hi, my name is Chris and I'm 29 years old");
+'use strict'
 
+function yearOfBirth(thisYear=2017, age) {
+  if ( age > 0 ) {
+    return thisYear - age;
+  } else {
+    throw new Error('Cannot have age less than zero!')
+  }
+}
+function whoAmI(name, age) {
+  console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
+  console.log(`I was born in ${yearOfBirth(2017,143)}.`);
+}
+
+try {
+  whoAmI('Brad', 43);
+} catch(e) {
+  console.error(e.message);
+}
